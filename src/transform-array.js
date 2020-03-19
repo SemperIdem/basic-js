99389be52d77d386120ddb2033d0f087d3f52056
SemperIdem <faintler915@gmail.com>
2020-03-20 00:01:13 +0400

    feat: add solution transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    console.log("GAVNO");
+    if (!(arr instanceof Array)) { throw Error; }
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof (arr[i]) == 'string') {
+            switch(arr[i]) {
+                case '--discard-next':
+                    ++i;
+                    break;
+                case '--discard-prev':
+                    result.pop();
+                    break;
+                case '--double-next':
+                    if (i  < arr.length - 1) {
+                        result.push(arr[i + 1]);
+                    }
+                    break;
+                case '--double-prev':
+                  if (i > 0) {
+                        result.push(arr[i - 1]);
+                  }
+                 break;
+                default:
+                    result.push(arr[i]);
+                    break;
+            }
+        }
+        else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+  };
