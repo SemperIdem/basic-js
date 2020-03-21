8afd58c443314da7bf567a40c473dc93257a5ed4
SemperIdem <faintler915@gmail.com>
2020-03-21 18:30:55 +0400

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options.addition === null || (options.addition && typeof options.addition != 'string')) {
+        options.addition = String(options.addition);
+    }
+
+
+    if (!options.separator) {
+        options.separator = '+';
+      }
+    if (options.addition && !options.additionSeparator)
+      {
+        options.additionSeparator = '|'
+      }
+    let additions = Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+    let strings = Array(options.repeatTimes).fill(str + additions).join(options.separator);
+    return strings;
 };
   
